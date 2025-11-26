fca2deca1cbbb670ebe7b67ccd8bdde0316d2a7e
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-11-26 16:20:43 +0200

    feat(database): add religions table and unify lookup tables structure
    - Added religions table similar to nationalities and blood_types.
    - Prepared table for student and parent relations.
    - Ensured consistent schema across lookup tables.

---
+++ app/Models/Religions.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Religions extends Model
+{
+    use HasTranslations;
+    public array $translatable = ['name'];
+
+    protected $fillable = ['name'];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         // $this->call(UsersSeeder::class);
-        // $this->call(TypeBloodSeeder::class);
+        $this->call(TypeBloodSeeder::class);
         $this->call(NationalitieSeeder::class);
+        $this->call(ReligionSeeder::class);
     }
 }
